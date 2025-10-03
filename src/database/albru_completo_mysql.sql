05d421137598d80827c8da043b83cf021c015da4
Dario <dczl0603@gmail.com>
2025-10-03 02:27:22 -0500

    Avance

--- src/database/albru_completo_mysql.sql
+++ src/database/albru_completo_mysql.sql
@@ -421,6 +421,3 @@ ORDER BY c.fecha_datos_completos ASC;
 -- LEFT JOIN asesores a ON h.usuario_id = a.id 
 -- WHERE cliente_id = 1 ORDER BY fecha DESC;
 
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY 'tu_contraseña' WITH GRANT OPTION;
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
-FLUSH PRIVILEGES;
